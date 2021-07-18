78021249bbb813c92af4be6ce1465dac9c90c3d3
alsxm <tulip23@naver.com>
2021-07-18 17:12:12 +0900

    [FEAT] add idButton, pwButton

--- script.js
+++ script.js
@@ -1,16 +1,38 @@
-const button = document.getElementById("button");
+const idButton = document.getElementById("id-button");
+const pwButton = document.getElementById("pw-button");
+const mainButton = document.getElementById("main-button");
 const greeting = document.getElementById("greeting");
-        
-button.addEventListener('click', function(event){
-    if(button.innerHTML === "열기"){
-        alert("HeLLoOoOoOo");
-        greeting.style.visibility = "visible";
-        button.innerHTML = "닫기"
+
+function open(){
+    alert(`HeLLoOoOoOo`);
+    greeting.style.visibility = "visible";
+    mainButton.innerHTML = "닫기";
+}
+
+function close(){
+    alert("ByYyYye");
+    greeting.style.visibility = "hidden";
+    mainButton.innerHTML = "열기"
+}
+
+function needInput(input){
+    alert(`${input}를 입력하세요`);
+}
+
+mainButton.addEventListener('click', function(event){
+    if(mainButton.innerHTML === "열기"){
+        open();
     }
     else{
-        alert("ByYyYye");
-        greeting.style.visibility = "hidden";
-        button.innerHTML = "열기"
+        close();
     }
         
+});
+
+idButton.addEventListener('click', function(event){
+    needInput("아이디");
+});
+
+pwButton.addEventListener('click', function(event){
+    needInput("비밀번호");
 });
